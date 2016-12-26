ad32b91028f4b306b5b49be93ac6b3e461eab8ff
langrenbule <546024423@qq.com>
2016-12-26 23:44:40 +0800

    (+)声音加载工具类

--- zerolib/src/main/java/com/deity/zerolibgdx/voice/VoiceUtils.java
+++ zerolib/src/main/java/com/deity/zerolibgdx/voice/VoiceUtils.java
@@ -1,9 +1,22 @@
 package com.deity.zerolibgdx.voice;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+
 /**
  * 声音工具集，对声音进行二次额封装
  * Created by Deity on 2016/12/26.
  */
 
 public class VoiceUtils {
+
+    /**加载音乐*/
+    public static Music loadMusic(String mFileMusicPath){
+        return Gdx.app.getAudio().newMusic(Gdx.files.internal(mFileMusicPath));
+    }
+
+    public static Sound loadSound(String mFileSoundPath){
+        return Gdx.app.getAudio().newSound(Gdx.files.internal(mFileSoundPath));
+    }
 }
