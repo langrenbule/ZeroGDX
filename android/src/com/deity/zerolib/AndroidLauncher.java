ec17e165ba26abd7ab9bb3b5bff67f52014f9806
langrenbule <546024423@qq.com>
2016-12-26 23:34:30 +0800

    Initial commit

---
+++ android/src/com/deity/zerolib/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.deity.zerolib;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.deity.zerolib.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
---
+++ zerolib/src/main/java/com/deity/zerolibgdx/voice/VoiceUtils.java
@@ -0,0 +1,9 @@
+package com.deity.zerolibgdx.voice;
+
+/**
+ * 声音工具集，对声音进行二次额封装
+ * Created by Deity on 2016/12/26.
+ */
+
+public class VoiceUtils {
+}
